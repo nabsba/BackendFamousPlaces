ff657ea59d9f6ed06c59e56a97abd93920857313
nabsba <ns-dev@outlook.com>
2025-02-06 10:36:01 +0000

    refactor(auth/models): destructure "handleRegisterUser"

--- src/auth/models/authentification.ts
+++ src/auth/models/authentification.ts
@@ -1,9 +1,8 @@
 import { ERROR_MESSAGES, logErrorAsyncMessage, logMessage } from '../../common';
-import { returnToken } from '../../jwt';
 import { handleRefreshToken } from '../../jwt';
-
 import prismaClientDB from '../../lib/prismadb';
 import { LoginArgs, registerUser } from '../types/types';
+import {isUserInTheDB, createUserInTheDB } from '../services/authentifications';
 
 const handleLogin = async (data: LoginArgs) => {
   try {
@@ -20,47 +19,16 @@ const handleLogin = async (data: LoginArgs) => {
   }
 };
 
-const handleRegisterUser = async (data: registerUser) => {
+
+
+const handleRegisterUser = async (userInfo: registerUser) => {
   try {
     return await prismaClientDB.$transaction(async (tx) => {
-      const account = await tx.account.findUnique({
-        where: {
-          provider_providerAccountId: {
-            provider: data.data.providerAccountId,
-            providerAccountId: data.data.providerAccountId,
-            userId: data.data.id,
-          },
-        },
-      });
-
+      const account = await isUserInTheDB(tx, userInfo);
       if (account) {
-        return await handleRefreshToken(data.data);
+        return await handleRefreshToken(userInfo.data);
       }
-
-      const clientRole = await tx.role.findUnique({
-        where: { roleName: 'client' },
-      });
-      if (!clientRole) {
-        throw new Error('Default role "client" not found');
-      }
-      const token = returnToken(data.data);
-      await tx.user.create({
-        data: {
-          id: data.data.id,
-          email: data.data.email,
-          name: data.data.userName, // Optional, set as per the data
-          roleId: clientRole.id, // Make sure to reference a valid role ID
-          accounts: {
-            create: {
-              type: data.data.type,
-              provider: data.data.providerAccountId,
-              providerAccountId: data.data.providerAccountId,
-              refresh_token: token, // Optional
-            },
-          },
-        },
-      });
-      return token;
+      return await createUserInTheDB(tx, userInfo);
     });
   } catch (error) {
     logMessage(`${logErrorAsyncMessage('auth/models/authentification', `${ERROR_MESSAGES.REGISTER_USER}:`)},
@@ -68,6 +36,9 @@ const handleRegisterUser = async (data: registerUser) => {
     throw new Error(ERROR_MESSAGES.REGISTER_USER);
   }
 };
+
+
+
 
 const handleRegisterUserWithCookie = async (data: registerUser) => {
   try {
